26b20aed1bd92ed3bd96c3193858e0529fc6047f
AnjaliK-123 <68889051+AnjaliK-123@users.noreply.github.com>
2020-08-12 16:14:37 +0530

    Email

--- webapi-develop/SRMAPI/Data/SqlRequestRepo.cs
+++ webapi-develop/SRMAPI/Data/SqlRequestRepo.cs
@@ -21,6 +21,45 @@ namespace SRMAPI.Data
                 throw new ArgumentNullException(nameof(request));
             }
             _context.Request.Add(request);
+             var email1 = context.Employees.FirstOrDefault(e => e.Id == request.CreatedEmpId).EmailId;
+            var AdminEmail = context.Employees.FirstOrDefault(e =>  e.RoleId == (context.Roles.FirstOrDefault(r => r.Role1.Equals("Admin")).Id)).EmailId;
+            var fromAddress = new MailAddress("anjalikhadake888@gmail.com", "My Name");
+            var toAddress = new MailAddress((email1).ToString(),  (AdminEmail).ToString());
+            const string fromPassword = "khadake27101998";
+            const string subject = "Service Request";
+            string body = "Request Created!" +
+                            "\nRequest Id: " + request.Id +
+                                "\nRequest Created by: " + context.Employees.FirstOrDefault(e => e.Id == request.CreatedEmpId).FirstName +
+                                "\nDepartment: " + context.Department.FirstOrDefault(d => d.Id == request.DepartmentId).Name +
+                                 "\nCategory: " + context.Category.FirstOrDefault(c => c.Id == request.CategoryId).Name +
+                                  "\nSubcategory: " + context.Category.FirstOrDefault(s => s.Id == request.SubCategoryId).Name +
+                                  "\nTitle :" + request.Title +
+                                  "\nSummary :" + request.Summary;
+
+           
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                Timeout = 20000
+
+            };
+
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+
+                smtp.Send(message);
+            }
+
         }
 
         public void DeleteRequest(Request request)
